a140f0613e5e147c7daaba7a0709583aee3dc129
Satyam <ysatyam9627@gmail.com>
2025-07-31 04:02:09 +0530

    .

--- apps/api-gateway/src/app/auth/auth.controller.ts
+++ apps/api-gateway/src/app/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Headers, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller,Res, Inject, Post, Req, UseGuards, UnauthorizedException } from '@nestjs/common';
 import { MICROSERVICE } from '../../constant';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '../../guards/auth/auth.guard';
+import * as express from 'express'; 
+import { firstValueFrom } from 'rxjs';
+import { User } from './decorator/user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -16,19 +19,38 @@ export class AuthController {
     }
 
     @Post('login')
-    async login(@Body() body:LoginUserDto){
-        return this.authServiceClient.send("auth-loginUser",body)
+    async login(@Body() body:LoginUserDto,@Res({ passthrough: true }) res: express.Response){
+        const token = await firstValueFrom( this.authServiceClient.send("auth-loginUser",body))
+
+        res.cookie('refresh-token',token.refreshToken,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV !== 'development',
+            path: '/auth/refresh',
+            expires: new Date(Date.now()+ 7 * 24 * 60 * 60 * 1000)
+        })
+
+        return {accessTokoen: token.accessToken}
     }
 
     @UseGuards(AuthGuard)
     @Post('logout')
-    async logout(@Req() req){
-        const userId = req.user.userId
-        return this.authServiceClient.send("auth-logout",userId)
+    async logout(@User() user:{userId:string}) {
+        const userId = user.userId; 
+        return this.authServiceClient.send("auth-logout", userId);
     }
 
     @Post('refresh')
-    async refreshToken(@Body() body:{ refreshToken: string}){
-        return this.authServiceClient.send("auth-refreshtoken",body.refreshToken)
+    async refreshToken(@Req() req: express.Request, @Res({ passthrough: true }) res: express.Response){
+        const refreshToken = req.cookies['refresh-token']
+        if(!refreshToken) throw new UnauthorizedException("refresh token not found!")
+
+        const token = await firstValueFrom( this.authServiceClient.send("auth-refreshtoken",refreshToken))
+        res.cookie('refresh-token',token.refreshToken,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV !== 'development',
+            path: '/auth/refresh',
+            expires: new Date(Date.now()+ 7 * 24 * 60 * 60 * 1000)
+        })
+        return {accessTokoen: token.accessToken}
     }
 }
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,7 @@ async function bootstrap() {
       transform:true
     })
   )
+  app.use(cookieParser())
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
