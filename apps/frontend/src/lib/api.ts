6ffc74b3cde253dcd7b320ffd17c56ba9a0c530a
Satyam <ysatyam9627@gmail.com>
2025-08-10 22:12:26 +0530

    .

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+//todo: cors error fix it 
+
 export const api = axios.create({
   baseURL: 'http://localhost:3000/api/', // The URL of your API Gateway
   withCredentials: true, // This tells axios to send cookies with requests
